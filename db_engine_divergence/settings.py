dcc9ad3790fddf7f43afb0d25a37cd3311293a62
Wesley George <wesley.george@gmail.com>
2013-11-04 15:35:46 -0500

    Set USE_TZ=True, since the initial_data.json fixture, necessary for the example, does specify timezones

--- db_engine_divergence/settings.py
+++ db_engine_divergence/settings.py
@@ -20,7 +20,8 @@ SQLITE = {
     }
 }
 
-DATABASES=MYSQL
+DATABASES=SQLITE
+USE_TZ=True
 
 # immaterial for this example, but manage.py won't run without it.
 SECRET_KEY = 'fcmz(=^=9wjk(2lr*_^wvu2sy&0)h7xa5ck%0tea43-vc6n%pt'
